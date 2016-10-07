1e1805d9a889c0da735a9d0a351b34235f5d2336
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-10-07 15:27:14 +0100

    Adding library to app.
    Adding to gitignore

--- library/models.py
+++ library/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Article(models.models):
+class Article(models.Model):
    title = models.TextField()
    author = models.TextField()
    date = models.DateTimeField()
    abstract = models.TextField()
-   
+
